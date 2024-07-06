7958634d297c75a4c0eb3ecc3be44fb4cf04521c
Jacob Eke <jacob.eke8@gmail.com>
2024-07-06 20:06:02 +0100

    Added more tests

--- tests/promise.test.ts
+++ tests/promise.test.ts
@@ -1,8 +1,18 @@
 import { toAsyncFunction } from "../src";
 
-test("adds two numbers correctly", () => {
-	const promise = toAsyncFunction(() => false);
-	promise().then((value) => {
+test("should return a function", () => {
+	const fn = toAsyncFunction(() => 1);
+	expect(fn).toBeInstanceOf(Function);
+});
+
+test("should return a promise", () => {
+	const fn = toAsyncFunction(() => 1);
+	expect(fn()).toBeInstanceOf(Promise);
+});
+
+test("should return the correct value", () => {
+	const fn = toAsyncFunction((boolean) => boolean);
+	fn(false).then((value) => {
 		expect(value).toBe(false);
 	});
 });

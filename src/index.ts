051945cdf4261f95ffa29a7ee12cb318e57b9a33
Jacob Eke <jacob.eke8@gmail.com>
2025-10-19 20:30:25 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ export type AsyncCallable<T extends Callable> = (
 	...args: Parameters<T>
 ) => Promise<Awaited<ReturnType<T>>>;
 
-export function toAsync<Fn extends Callable>(fn: Fn) {
+export function toAsync<Fn extends Callable>(fn: Fn): AsyncCallable<Fn> {
 	return async (...args: Parameters<Fn>) => {
 		const result = fn(...args);
 		return result instanceof Promise ? result : Promise.resolve(result);
 	};
+}
+
+export function toAsyncFunction<T extends Callable>(
+	fn: Parameters<typeof toAsync<T>>[0]
+): ReturnType<typeof toAsync<T>> {
+	return toAsync<T>(fn);
 }
